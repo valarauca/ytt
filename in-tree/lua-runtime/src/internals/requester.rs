3fd29ecdf417ec43311fe2171df090ff8be71464
william cody laeder <valarauca@valarauca.DESKTOP>
2026-03-27 13:26:33 -0700

    Fix a few lingering errors

--- in-tree/lua-runtime/src/internals/requester.rs
+++ in-tree/lua-runtime/src/internals/requester.rs
@@ -9,42 +9,55 @@ use tower::{Service,ServiceExt};
 use mlua::{UserData,UserDataMethods,Lua,UserDataRef};
 use futures_util::{FutureExt,TryFutureExt};
 
-use lua_integration::{LuaKind};
+use lua_integration::{LuaKind,LuaIntegration};
 use openrouter::completions::{Request as ORRequest,Response as ORResponse};
 use runtime_model::adapters::{BoxCloneSyncService};
 
 
 #[derive(Clone)]
 pub struct ORService {
-    internals: BoxCloneSyncService<ORRequest,ORResponse,Error>,
+    internals: BoxCloneSyncService<ORRequest,ORResponse,anyhow::Error>,
 }
-impl From<BoxCloneSyncService<ORRequest,ORResponse,Error>> for ORService {
-    fn from(internals: BoxCloneSyncService<ORRequest,ORResponse,Error>) -> Self {
+impl From<BoxCloneSyncService<ORRequest,ORResponse,anyhow::Error>> for ORService {
+    fn from(internals: BoxCloneSyncService<ORRequest,ORResponse,anyhow::Error>) -> Self {
         Self { internals }
     }
 }
-
-#[derive(Clone,PartialEq,LuaKind)]
-pub enum Response {
-    Success(ORResponse),
-    Error(String),
-}
-
 impl UserData for ORService {
     fn add_methods<M: UserDataMethods<Self>>(m: &mut M) {
-        m.add_async_method("call", |_: &L, this: UserDataRef<Self>, A: UserDataRef<ORRequest>| -> Pin<Box<dyn Future<Output=mlua::Result<Response>> + Send + 'static>> {
-            let mut this: Self = this.deref().clone();
-            Box::pin(async move {
-                let mut service: BoxCloneSyncService<ORRequest,ORResponse,Error> = this.internals;
-                let out = match service.ready().and_then(move |x| x.call(a.deref().clone())).await {
-                    Ok(x) => Response::Success(ORResponse),
-                    Err(e) => {
-                        // TODO: log this
-                        Response::Error(format!("{:?}", e))
-                    }
-                };
-                Ok(out)
-            })
-        })
+        m.add_async_method("make_full_request", |_: mlua::Lua, this: UserDataRef<Self>, req: UserDataRef<ORRequest>| -> Pin<Box<dyn Future<Output=mlua::Result<ORResponse>> + Send + 'static>> {
+            let service = this.deref().internals.clone(); 
+            let request: ORRequest = req.deref().clone();
+            invoke_service(service, request, mlua::Error::external)
+        });
     }
 }
+
+
+pub type PBFut<T> = Pin<Box<dyn Future<Output=T> + Send + 'static>>;
+pub fn invoke_service<S,R,E>(mut service: S, request: R, change: fn(<S as Service<R>>::Error) -> E) -> PBFut<Result<<S as Service<R>>::Response,E>>
+where
+    S: Service<R> + ServiceExt<R> + Send + 'static,
+    <S as Service<R>>::Future: Send + 'static,
+    <S as Service<R>>::Response: Send + 'static,
+    <S as Service<R>>::Error: Send + 'static,
+    E: Send + 'static,
+    R: Send + 'static,
+{
+    let err_lambda = move |x: Result<<S as Service<R>>::Response,<S as Service<R>>::Error>| -> Result<<S as Service<R>>::Response,E> {
+        x.map_err(change)
+    };
+    Box::pin(async move {
+        service.ready().then(move |r: Result<&mut S,<S as Service<R>>::Error>| {
+            match r {
+                Ok(srv) => {
+                    srv.call(request).map(err_lambda).left_future()
+                }
+                Err(e) => {
+                    std::future::ready(Err(change(e))).right_future()
+                }
+            }
+        }).await
+    })
+}
+
--- yttrium/src/main.rs
+++ yttrium/src/main.rs
@@ -1,9 +1,10 @@
 
 
-
+/*
 pub mod endpoints;
 pub mod config;
 pub mod signal;
+*/
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -11,6 +12,7 @@ async fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-
+/*
 async fn primary_loop() {
 }
+*/
